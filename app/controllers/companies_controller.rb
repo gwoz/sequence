0d9eb6badba1a8ca4e5cc4157bdcb9415cb99e5b
Mike Gwozdek <email": null>
2016-04-16 12:56:00 -0400

    Add responders gem and ability fro companies and patterns to respond to html and json

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,14 @@
 class CompaniesController < ApplicationController
 
+  respond_to :html, :json
+
   def index
     @companies = Company.all
+    respond_with(@companies)
+  end
+
+  def new
+    @company = Company.new
   end
 
 end
--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,11 +1,15 @@
 class PatternsController < ApplicationController
 
+  respond_to :html, :json
+
   def index
     @patterns = Pattern.all
+    respond_with(@patterns)
   end
 
   def show
     @pattern = Pattern.find_by(company_id: params[:id])
+    respond_with(@pattern)
   end
 
 end
