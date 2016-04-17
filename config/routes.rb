54b543a39dc19c1fa009c30bc41ab01c1672a6d8
gwoz <mikegwozdek@gmail.com>
2016-04-17 12:06:42 -0400

    Remove root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :companies, defaults: { format: :json } do
     resources :projects, defaults: { format: :json } do
-      resources :patterns
+      resources :patterns, defaults: { format: :json }
       end
     resources :patterns, defaults: { format: :json }
     resources :clients, defaults: { format: :json }
