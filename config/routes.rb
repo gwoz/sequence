3c512c3c7f5c0c1652d9fae8099af2554e061f0f
gwoz <mikegwozdek@gmail.com>
2016-04-17 11:50:22 -0400

    Add route for the patterns of a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :companies, defaults: { format: :json } do
-    resources :projects, defaults: { format: :json }
+    resources :projects, defaults: { format: :json } do
+      resources :patterns
+      end
     resources :patterns, defaults: { format: :json }
     resources :clients, defaults: { format: :json }
   end
