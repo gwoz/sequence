2d9a5ce859ee3274d7d38a5d17c182a495d78f5f
gwoz <mikegwozdek@gmail.com>
2016-04-17 11:13:42 -0400

    Add routes for clients and companies

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'companies#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -14,10 +12,19 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
+
     resources :companies do
-      resources :patterns do
-      end
+      resources :projects
+      resources :patterns
+      resources :clients
     end
+
+    resource :clients do
+      resources :projects
+      resources :patterns
+    end
+
+
 
   # Example resource route with options:
   #   resources :products do
