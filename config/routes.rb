73ae6a57289ad6d4caae28586d83ffcfbfd5d34e
gwoz <mikegwozdek@gmail.com>
2016-04-17 21:07:04 -0400

    Remove nesting of routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root 'main#index'
 
   resources :companies do
-    resources :projects do
-      resources :patterns
-      end
     resources :patterns
     resources :clients
   end
